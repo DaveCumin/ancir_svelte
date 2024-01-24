50fcfc97a803c5c08199ce6d13699c46578386b2
David Cumin <david.cumin@gmail.com>
2024-01-24 16:23:32 +1300

    Zero tabs fixed. Thanks to @khromov

--- src/charts/Raw/Raw_controls.svelte
+++ src/charts/Raw/Raw_controls.svelte
@@ -3,74 +3,76 @@
   import { graphs, activeGraphTab } from "../../store";
 </script>
 
-<div class="chartControls">
-  <div class="sliderContainer">
-    <Slider
-      min={100}
-      max={1000}
-      limits={[5, Infinity]}
-      label="Width:"
-      bind:value={$graphs[$activeGraphTab].params.width}
-    />
-  </div>
+{#if $activeGraphTab >= 0}
+  <div class="chartControls">
+    <div class="sliderContainer">
+      <Slider
+        min={100}
+        max={1000}
+        limits={[5, Infinity]}
+        label="Width:"
+        bind:value={$graphs[$activeGraphTab].params.width}
+      />
+    </div>
 
-  <div class="sliderContainer">
-    <Slider
-      min={10}
-      max={3000}
-      limits={[5, Infinity]}
-      label="Height: "
-      bind:value={$graphs[$activeGraphTab].params.height}
-    />
-  </div>
+    <div class="sliderContainer">
+      <Slider
+        min={10}
+        max={3000}
+        limits={[5, Infinity]}
+        label="Height: "
+        bind:value={$graphs[$activeGraphTab].params.height}
+      />
+    </div>
 
-  <div class="sliderContainer">
-    <label for="val">X-axis: </label>
-    <input
-      type="text"
-      id="min"
-      bind:value={$graphs[$activeGraphTab].params.xAxisLabel}
-    />
-  </div>
-  <div class="sliderContainer">
-    <Slider
-      min={-10}
-      max={30}
-      label="Min X:"
-      bind:value={$graphs[$activeGraphTab].params.xDomainMin}
-    />
-  </div>
-  <div class="sliderContainer">
-    <Slider
-      min={-10}
-      max={30}
-      label="Max X:"
-      bind:value={$graphs[$activeGraphTab].params.xDomainMax}
-    />
-  </div>
+    <div class="sliderContainer">
+      <label for="val">X-axis: </label>
+      <input
+        type="text"
+        id="min"
+        bind:value={$graphs[$activeGraphTab].params.xAxisLabel}
+      />
+    </div>
+    <div class="sliderContainer">
+      <Slider
+        min={-10}
+        max={30}
+        label="Min X:"
+        bind:value={$graphs[$activeGraphTab].params.xDomainMin}
+      />
+    </div>
+    <div class="sliderContainer">
+      <Slider
+        min={-10}
+        max={30}
+        label="Max X:"
+        bind:value={$graphs[$activeGraphTab].params.xDomainMax}
+      />
+    </div>
 
-  <div class="sliderContainer">
-    <label for="val">Y-axis: </label>
-    <input
-      type="text"
-      id="min"
-      bind:value={$graphs[$activeGraphTab].params.yAxisLabel}
-    />
+    <div class="sliderContainer">
+      <label for="val">Y-axis: </label>
+      <input
+        type="text"
+        id="min"
+        bind:value={$graphs[$activeGraphTab].params.yAxisLabel}
+      />
+    </div>
+    <div class="sliderContainer">
+      <Slider
+        min={-10}
+        max={30}
+        label="Min Y:"
+        bind:value={$graphs[$activeGraphTab].params.yDomainMin}
+      />
+    </div>
+    <div class="sliderContainer">
+      <Slider
+        min={-10}
+        max={30}
+        label="Max Y:"
+        bind:value={$graphs[$activeGraphTab].params.yDomainMax}
+      />
+    </div>
   </div>
-  <div class="sliderContainer">
-    <Slider
-      min={-10}
-      max={30}
-      label="Min Y:"
-      bind:value={$graphs[$activeGraphTab].params.yDomainMin}
-    />
-  </div>
-  <div class="sliderContainer">
-    <Slider
-      min={-10}
-      max={30}
-      label="Max Y:"
-      bind:value={$graphs[$activeGraphTab].params.yDomainMax}
-    />
-  </div>
-</div>
+{/if}
